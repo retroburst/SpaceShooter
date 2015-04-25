bb4cf123a06fe5daaa008bad8d2e8f988b0f3a41
Andrew D <retroburst@gmail.com>
2015-04-25 22:06:32 +0900

    Completed

--- Assets/Scripts/DetsoryByContact.cs
+++ Assets/Scripts/DetsoryByContact.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class DetsoryByContact : MonoBehaviour {
 	public GameObject explosion = null;
 	public GameObject playerExplosion = null;
+	public int points = 10;
+	private GameController gameController = null;
+
+	void Start(){
+		GameObject gameControllerGameObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerGameObject != null) {
+			gameController = gameControllerGameObject.GetComponent<GameController>();
+			if(gameController == null) Debug.Log("Could not get a reference to GameController script.");
+		}
+	}
 
 	void OnTriggerEnter(Collider otherCollider){
 		Debug.Log ("otherCollider name is: " + otherCollider.name);
@@ -12,6 +22,7 @@ public class DetsoryByContact : MonoBehaviour {
 		}
 		Instantiate (explosion, transform.position, transform.rotation);
 		if(otherCollider.tag == "Player"){
+			gameController.PlayerDestroyed();
 			Instantiate(playerExplosion, otherCollider.transform.position, otherCollider.transform.rotation);
 		}
 		Destroy(otherCollider.gameObject);
@@ -19,6 +30,7 @@ public class DetsoryByContact : MonoBehaviour {
 			Destroy (transform.parent.gameObject);
 		}
 		Destroy (gameObject);
+		gameController.AddScore (points);
 	}
 
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
@@ -8,29 +9,62 @@ public class GameController : MonoBehaviour {
 	public float asteroidSpawnWait = 0.0f;
 	public float asteroidSpawnStartWait = 0.0f;
 	public float asteroidWaveWait = 0.0f;
+	private int score = 0;
+	public Text scoreText = null;
+	public Text restartText = null;
+	public Text gameOverText = null;
+	private bool gameOver = false;
+	private bool restart = false;
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(SpawnAsteroidWaves ());
+		UpdateScore ();
+		restartText.text = string.Empty;
+		gameOverText.text = string.Empty;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (restart && Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
 	}
 
 	IEnumerator SpawnAsteroidWaves(){
 		yield return new WaitForSeconds(asteroidSpawnStartWait);
-		while(true)
+
+		while(!gameOver)
 		{
-			for(int i=0; i < asteroidCount; i++)
+			for(int i=0; i < asteroidCount && !gameOver; i++)
 			{
 				Vector3 spawnPosition = new Vector3 (Random.Range(-asteroidSpawn.x, asteroidSpawn.x), asteroidSpawn.y, asteroidSpawn.z);
 				Instantiate (asteroid, spawnPosition, Quaternion.identity);
 				yield return new WaitForSeconds(asteroidSpawnWait);
 			}
-			yield return new WaitForSeconds(asteroidWaveWait);
+
+
+			if(!gameOver) yield return new WaitForSeconds(asteroidWaveWait);
 		}
+
+			restartText.text += "Restart game by pressing the 'R' key...";
+			restart = true;
 	}
 
+	void UpdateScore()
+	{
+		scoreText.text = string.Format("Score: {0:N0}", score);
+	}
+
+	public void AddScore(int points)
+	{
+		score += points;
+		UpdateScore ();
+	}
+
+	public void PlayerDestroyed()
+	{
+		gameOverText.text = "Game Over Man!";
+		gameOver = true;
+	}
 }
