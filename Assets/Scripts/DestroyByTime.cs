533cfb52f9ff78d0836b8c924c4a8e5de2dc9fde
Andrew D <retroburst@gmail.com>
2015-07-23 12:16:06 +0900

    Clean up
    This file didn’t get checked in for some reason.

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Destroy by time.
+/// Destroys game objects based
+/// on a given lifetime. 
+/// </summary>
+public class DestroyByTime : MonoBehaviour
+{
+	public float lifetime;
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	private void Start ()
+	{
+		Destroy (gameObject, lifetime);
+	}
+	
+}
